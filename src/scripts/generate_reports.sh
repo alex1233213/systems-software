6bb161d4da81a0589279d0f75cab104ffd20a824
alex <alexanderbulgari@gmail.com>
2021-11-04 17:19:07 +0000

    generate log

--- src/scripts/generate_reports.sh
+++ src/scripts/generate_reports.sh
@@ -3,5 +3,5 @@
 audit_file="audit-report-$(date +'%d-%m-%Y').txt"
 
 #generate audit log 
-sudo ausearch  -ts today -f /home/alex/Desktop/assignment/managers_xml_upload | sudo aureport -f -i  > /home/alex/Desktop/assignment/manufacturing_company_reports/$audit_file
+sudo ausearch  -ts today -k xml_upload_watch | sudo aureport -f -i  > /home/alex/Desktop/assignment/manufacturing_company_reports/$audit_file
 
